1b5bc03e231e5f5fa6560ad9377ad70880d64e9f
sanchayana2007 <sanchayana2007@gmail.com>
2014-08-04 13:37:57 +0530

    Added

---
+++ app.js
@@ -0,0 +1,48 @@
+// mongoose setup
+require('./data/models/product');
+ 
+var express = require( 'express' );
+var routes  = require( './routes' );
+var http    = require( 'http' );
+var path    = require( 'path' );
+var app     = express();
+var engine  = require( 'ejs-locals' );
+ 
+// all environments
+app.set( 'port', process.env.PORT || 3001 );
+app.engine( 'ejs', engine );
+app.set( 'views', path.join( __dirname, 'views' ));
+app.set( 'view engine', 'ejs' );
+app.use( express.favicon());
+app.use( express.logger( 'dev' ));
+app.use( express.bodyParser());
+app.use( express.json());
+app.use( express.urlencoded());
+app.use( express.methodOverride());
+	//Session maintainence stuffs
+		app.use(express.cookieParser('my secret string'));
+		app.use(express.session({
+				secret: 'my secret string',
+				maxAge: 3600000
+		}));
+app.use( app.router );
+app.use( express.static( path.join( __dirname, 'public' )));
+ 
+// development only
+if ( 'development' == app.get( 'env' )) {
+  app.use( express.errorHandler());
+}
+
+// Routes are separeted and kept in a different route folder having the GET/POSTs
+//app.get('/', routes.index);
+require('./routes/index')(app);
+require('./routes/users')(app);
+require('./routes/session')(app);
+require('./routes/Products')(app);
+require('./routes/release')(app);
+require('./routes/feature')(app);
+require('./routes/meetings')(app);
+//require('./routes/meetinglist')(app);
+http.createServer( app ).listen( app.get( 'port' ), function(){
+  console.log( 'Express server listening on port ' + app.get( 'port' ));
+} );
